de16f2515c4279f5a36be82782eab7d5acb19098
ruibao <18132890005@163.com>
2023-04-07 11:40:41 +0800

    second commit

--- src/main/java/com/radiateall777/git/GitTest.java
+++ src/main/java/com/radiateall777/git/GitTest.java
@@ -3,5 +3,6 @@ package com.radiateall777.git;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
